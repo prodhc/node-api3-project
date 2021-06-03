31ff94322564855ce0aa5d11ba1bed8754beed73
prodhc <zionywood@gmail.com>
2021-06-02 18:54:01 -0700

    post endpoint

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,22 +1,45 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
-      console.log(`[${timestamp}] ${method} to ${url}`)
-    next()
-    }
+  console.log(`[${timestamp}] ${method} to ${url}`)
+  next()
+  }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUserID middleware')
-    next()
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'no user found',
+      })
+    }
+    else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'server error',
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validateUser middleware')
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  }
+  else {
+    req.name = name.trim()
     next()
+  }
 }
 
 function validatePost(req, res, next) {
